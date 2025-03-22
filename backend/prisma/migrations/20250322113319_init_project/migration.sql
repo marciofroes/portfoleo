052ec76ada1930bb7a0dfb8c5cf1a60d547ba64d
marcio.froes <lopes.froes@gmail.com>
2025-03-22 08:46:18 -0300

    init

--- backend/prisma/migrations/20250322113319_init_project/migration.sql
+++ backend/prisma/migrations/20250322113319_init_project/migration.sql
@@ -1,4 +1,29 @@
 -- CreateTable
+CREATE TABLE "tecnologias" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "tecnologias_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projetos" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagens" TEXT[],
+    "nivel" INTEGER NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+    "repositorio" TEXT NOT NULL,
+
+    CONSTRAINT "projetos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_ProjetoToTecnologia" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL,
